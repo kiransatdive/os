b318fcb2c7d5420de95e9672f5c5d2184e8d7cca
KIRAN SATDIVE <98806225+kiransatdive@users.noreply.github.com>
2022-11-23 00:48:45 +0530

    Update Orphan & Zombi reverse Order.c

--- OS practical codes/Orphan & Zombi reverse Order.c
+++ OS practical codes/Orphan & Zombi reverse Order.c
@@ -1,88 +1,126 @@
 
-2) ,15) 
+3) ,16)
 Process control system calls: The demonstration of FORK, EXECVE and WAIT system calls along with zombie and orphan states
-Implement the C program in which main program accepts the integers to be sorted. Main program uses the FORK system call to create a new process called a child process. Parent process sorts the integers using sorting algorithm and waits for child process using WAIT system call to sort the integers using any sorting algorithm. Also demonstrate zombie and orphan states.
+Implement the C program in which main program accepts an array. Main program uses the FORK system call to create a new process called a child process. Parent process sorts an array and passes the sorted array to child process through the command line arguments of EXECVE system call. The child process uses EXECVE system call to load new program which display array in reverse order
 
-#include <stdio.h>
+#include<stdio.h>
+#include<sys/types.h>
+#include<unistd.h>
+#include<stdlib.h>		
+void bass(int arr[30],int n)
+{
+	int i,j,temp;
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n-1;j++)
+		{
+			if(arr[j]>arr[j+1])
+			{
+				temp=arr[j];
+				arr[j]=arr[j+1];
+				arr[j+1]=temp;
+			}
+		}
+	}
+	printf("\n Ascending Soring \n");
+	for(i=0;i<n;i++)
+		printf("\t%d",arr[i]);
+	
+}
+
+void bdsc(int arr[30],int n)
+{
+	int i,j,temp;
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n-1;j++)
+		{
+			if(arr[j]<arr[j+1])
+			{
+				temp=arr[j];
+				arr[j]=arr[j+1];
+				arr[j+1]=temp;
+			}
+		}
+	}
+	printf("\n Descending Sorting \n");
+	for(i=0;i<n;i++)
+		printf("\t%d",arr[i]);
+	printf("\n\n");
+	
+}
+void forkeg()
+{
+	int arr[25],arr1[25],n,i,status;
+ 	printf("\nEnter Size : - \n");
+ 	scanf("%d",&n);
+ 	printf("\nEnter Array : - \n");
+ 	for(i=0;i<n;i++)
+    		scanf("%d",&arr[i]);
+ 	int pid=fork();
+  	if(pid==0)
+   	{
+      		printf("\nChild Id = %d\n",getpid());
+		bdsc(arr,n);
+		printf("\n Bubble Sorting \n");
+		printf("\n");
+       		for(i=0;i<n;i++)
+    			printf("\t %d,",arr[i]);
+    		printf("\n");
+     	}  
+  	else
+   	{
+     		sleep(10);
+     		
+      		printf("Parent Id = %d\n",getppid());
+     		bass(arr,n);
+      		printf("\n Bubble Sort : \n");
+  	   	printf("\n");
+      		for(i=0;i<n;i++)
+    			printf("%d,",arr[i]);
+    			printf("\b");
+    		system("ps -x");
+       } 
+ }    
+ 
+int main()
+ {
+   forkeg();
+   return 0;
+ }  
+
+
+Orphan Program Code:
+#include <stdio.h> 
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <stdlib.h>
 #include <unistd.h>
-
-void bubblesort()
-{
-int i,j,a[50],n,temp;
-printf("\nBubble sort\n");
-printf("Enter size of array\t");
-scanf("%d",&n);
-for(i=0;i<n;i++)
-{
-scanf("%d",&a[i]);
-}
-for(i=0;i<n;i++)
-{
-for(j=i+1;j<n;j++)
-{
-if(a[i]>a[j])
-{
-int temp;
-temp=a[i];
-a[i]=a[j];
-a[j]=temp;
-}
-}
-}
-printf("\nSorted array is\t");
-for(i=0;i<n;i++)
-{
-printf("%d\t",a[i]);
-}  
-}
-void selectionsort()
-{
-int i,j,a[50],n,min;
-printf("Selection sort\n");
-printf("\nEnter size of array\t");
-scanf("%d",&n);
-for(i=0;i<n;i++)
-{
-scanf("%d",&a[i]);
-}
- for (i = 0; i < n-1; i++)
- { 
- min= i;
- for (j = i+1; j < n; j++)
- if (a[j] < a[min])
- min= j;
-if(i<min)
-{
-int temp;
-temp=a[min];
-a[min]=a[i];
-a[i]=temp;
-}
-}
-printf("\nSorted array is\t");
-for(i=0;i<n;i++)
-{
-printf("%d\t",a[i]);
-}
-}
-
 int main()
-{
-pid_t pid;
-pid=fork();
-
-if(fork()==0)
-{
-bubblesort();
-}
-else
-{
-wait(NULL);
-selectionsort();
-}
-return 0;
+{ 
+ int pid;
+ pid=getpid(); 
+ printf("Current Process ID is : %d\n",pid); 
+ printf("\n[Forking Child Process ... ] \n"); 
+ pid=fork(); /* This will Create Child Process and Returns   Child's PID */
+ if(pid < 0) 
+ {    
+  printf("\nProcess can not be created ");
+ } 
+ else
+ {
+  if(pid==0)
+  { /* Child Process */
+   printf("\nChild Process is Sleeping ..."); 
+   sleep(5); /*Orphan Child's Parent ID is 1 */
+   printf("\nOrphan Child's Parent ID : %d",getppid()); 
+  }
+  else
+  { /* Parent Process */
+   printf("\nParent Process Completed ..."); 
+  } 
+ }
+ return 0; 
 }
 
 
