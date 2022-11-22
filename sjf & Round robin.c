9e631c5812506d2de28f662818e326b6258c4e8b
KIRAN SATDIVE <98806225+kiransatdive@users.noreply.github.com>
2022-11-23 02:16:58 +0530

    Update sjf & Round robin.c

--- sjf & Round robin.c
+++ sjf & Round robin.c
@@ -51,47 +51,57 @@ SJF:
 }
 
 Round Robin :
-#include <stdio.h>
-  int main() 
+#include<stdio.h>
+ 
+int main()
 {
-      int arrival_time[10], burst_time[10], temp[10];
-      int i, smallest, count = 0, time, limit;
-      double wait_time = 0, turnaround_time = 0, end;
-      float average_waiting_time, average_turnaround_time;
-      printf("Enter the Total Number of Processes:");
-      scanf("%d", &limit); 
-      printf("nEnter Details of %d Processesn", limit);
-      for(i = 0; i < limit; i++)
-      {
-            printf("nEnter Arrival Time:");
-            scanf("%d", &arrival_time[i]);
-            printf("Enter Burst Time:");
-            scanf("%d", &burst_time[i]); 
-            temp[i] = burst_time[i];
-      }
-      burst_time[9] = 9999;  
-      for(time = 0; count != limit; time++)
-      {
-            smallest = 9;
-            for(i = 0; i < limit; i++)
-            {
-                  if(arrival_time[i] <= time && burst_time[i] < burst_time[smallest] && burst_time[i] > 0)
-                  {
-                        smallest = i;
-                  }
-            }
-            burst_time[smallest]--;
-            if(burst_time[smallest] == 0)
-            {
-                  count++;
-                  end = time + 1;
-                  wait_time = wait_time + end - arrival_time[smallest] - temp[smallest];
-                  turnaround_time = turnaround_time + end - arrival_time[smallest];
-            }
-      }
-      average_waiting_time = wait_time / limit; 
-      average_turnaround_time = turnaround_time / limit;
-      printf("nnAverage Waiting Time:t%lfn", average_waiting_time);
-      printf("Average Turnaround Time:t%lfn", average_turnaround_time);
-      return 0;
+ 
+  int cnt,j,n,t,remain,flag=0,tq;
+  int wt=0,tat=0,at[10],bt[10],rt[10];
+  printf("Enter Total Process:\t ");
+  scanf("%d",&n);
+  remain=n;
+  for(cnt=0;cnt<n;cnt++)
+  {
+    printf("Enter Arrival Time and Burst Time for Process Process Number %d :",cnt+1);
+    scanf("%d",&at[cnt]);
+    scanf("%d",&bt[cnt]);
+    rt[cnt]=bt[cnt];
+  }
+  printf("Enter Time Quantum:\t");
+  scanf("%d",&tq);
+  printf("\n\nProcess\t|Turnaround Time|Waiting Time\n\n");
+  for(t=0,cnt=0;remain!=0;)
+  {
+    if(rt[cnt]<=tq && rt[cnt]>0)
+    {
+      t+=rt[cnt];
+      rt[cnt]=0;
+      flag=1;
+    }
+    else if(rt[cnt]>0)
+    {
+      rt[cnt]-=tq;
+      t+=tq;
+    }
+    if(rt[cnt]==0 && flag==1)
+    {
+      remain--;
+      printf("P%d]\t|\t%d\t|\t%d\n",cnt+1,t-at[cnt],t-at[cnt]-bt[cnt]);
+      wt+=t-at[cnt]-bt[cnt];
+      tat+=t-at[cnt];
+      flag=0;
+    }
+    if(cnt==n-1)
+      cnt=0;
+    else if(at[cnt+1]<=t)
+      cnt++;
+    else
+      cnt=0;
+  }
+  printf("\nAverage Waiting Time= %f\n",wt*1.0/n);
+  printf("Avg Turnaround Time = %f",tat*1.0/n);
+  
+  return 0;
 }
+
